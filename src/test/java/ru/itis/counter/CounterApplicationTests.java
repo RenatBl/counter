72926c4829d5add614b0c158f372d51ad896313c
Renat <renat.blinov@yandex.ru>
2020-06-18 19:58:56 +0300

    Added unit tests

--- src/test/java/ru/itis/counter/CounterApplicationTests.java
+++ src/test/java/ru/itis/counter/CounterApplicationTests.java
@@ -1,13 +1,33 @@
 package ru.itis.counter;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.itis.counter.utils.PageSavingUtil;
+import ru.itis.counter.utils.WordsFindingUtil;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
-class CounterApplicationTests {
+public class CounterApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testSavePage() {
+        PageSavingUtil.savePage("https://www.simbirsoft.com/");
+        boolean isExist = new File("D:/pages/simbirsoft.html").exists();
+
+        assertTrue(isExist);
     }
 
+    @Test
+    public void testGettingWords() {
+        Map<String, Integer> words = WordsFindingUtil.getWordsOnPage("https://www.simbirsoft.com/");
+
+        assertFalse(words.isEmpty());
+    }
 }
